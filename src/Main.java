da47801ee7e86a1b4db47455ad38632b42552438
colemaih <colemaih@mail.uc.edu>
2023-09-14 17:21:18 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int intSummerCost= 300;
+        int intFallCost= 200;
+        int intWinterCost= 400;
+        int intSpringCost= 200;
+      int intTotal= intSummerCost + intFallCost + intWinterCost + intSpringCost;
+        System.out.println("Your summer  maintenance cost is "+ intSummerCost );
+        System.out.println("Your fall  maintenance cost is "+ intFallCost );
+        System.out.println("Your winter  maintenance cost is "+ intWinterCost );
+        System.out.println("Your spring  maintenance cost is "+ intSpringCost );
+        System.out.println("Your total yearly coast is "+ intTotal);
+    }
+}
